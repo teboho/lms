48e2f2b15152eef38b2879728d6850fa75614291
teboho <tebohomakhalemele@outlook.com>
2024-03-13 09:56:01 +0200

    Added Home page starter code

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+import React from "react";
+
+export default function Home(): React.ReactNode {
+    return (
+        <>
+            <h1>Home</h1>
+            <Button text="Bang!" />
+        </>
+    );
+}
