ab83be354642947817afcb8a9bdad2ee4be46a2c
teboho <tebohomakhalemele@outlook.com>
2024-03-26 20:19:17 +0200

    axios

--- frontend/src/providers/SessionProvider/index.tsx
+++ frontend/src/providers/SessionProvider/index.tsx
@@ -5,50 +5,52 @@ import { sessionReducer } from "./reducer";
 import { postSessionErrorAction, postSessionRequestAction, postSessionSuccessAction } from "./actions";
 import { useRouter } from 'next/navigation';
 import { message } from "antd";
-
-const apiURL = process.env.NEXT_PUBLIC_API_URL;
+import axios from "axios";
+import { baseURL } from "../AuthProvider";
 
 export default function SessionProvider({ children }: { children: React.ReactNode }) {
     // we will make the state with the reducers
     const [sessionState, dispatch] = useReducer(sessionReducer, SESSION_INITIAL_STATE);
     const [messageApi, contextHolder] = message.useMessage();
 
-    
     const fail = (): void => {
         messageApi.open({
             type: "error",
             content: "Login Attempt Unsuccessful"
         })
     }
+
+    
+    const accessToken = localStorage.getItem("accessToken");
+    const instance = axios.create({
+        baseURL: baseURL,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
     
     function getCurrentLoginInformations(): void {
         // conduct the fetch and dispatch based on the response
-        const endpoint = apiURL + "api/services/app/Session/GetCurrentLoginInformations";
-        const accessToken = localStorage.getItem("accessToken");
-        fetch(endpoint, {
-            headers: {
-                "Content-Type": "*/*",
-                "Authorization": `Bearer ${accessToken}`
-            },
-        }).then(res => res.json())
-        .then(data => {
-            console.log("data", data);
-            if (data.success) {
-                const res: SessionUser = data.result.user;
-                dispatch(postSessionSuccessAction(res));
-                console.log("GetCurrentLoginInformations success");
-                // console.log(res)
+        const endpoint = "/api/services/app/Session/GetCurrentLoginInformations";
 
-            } else {
-                console.log("GetCurrentLoginInformations unsuccessful");
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            dispatch(postSessionErrorAction());
-        })
+        instance.get(endpoint)
+            .then(response => {
+                const data = response.data;
+                console.log("data", data);
+                if (data.success) {
+                    const res = data.result.user;
+                    dispatch(postSessionSuccessAction(res));
+                    console.log("GetCurrentLoginInformations success");
+                } else {
+                    console.log("GetCurrentLoginInformations unsuccessful");
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(postSessionErrorAction());
+            });
     }
-
 
     return (
         <SessionContext.Provider value={{ getInfo: getCurrentLoginInformations, sessionState }}>
