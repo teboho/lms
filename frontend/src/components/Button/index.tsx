8e8b7bf237c47a9156a9d5d40383c6529d843df0
teboho <13440702+teboho@users.noreply.github.com>
2024-04-12 10:15:55 +0200

    Delete frontend/src/components/Button directory

--- frontend/src/components/Button/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Button(props: { text: string }) : React.ReactNode {
-    // this is a generic button
-    return <button>{props.text}</button>;
-}
